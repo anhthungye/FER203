df38ab6a38ef0ca6e59733ba97b53a50ba894e93
anhthungye <nguyenthianhthu20005@gmail.com>
2025-08-11 10:01:34 +0700

    Add Slot 1

---
+++ Slot1/slot1-ex1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const myName = "Anh Thư"; 
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Hello React</h1>
+      <p>My name is {myName}</p>
+    </div>
+  );
+}
+
+export default App;
